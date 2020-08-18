d63858e611adc840e3d5d28a216c2c8e5066bd2d
AraqueGD <camiaraquecaro17@gmail.com>
2020-08-18 18:27:20 -0500

    [ADD]0x04. Sandpiles

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -8,18 +8,18 @@
  */
 void _print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
@@ -29,17 +29,17 @@ void _print_grid(int grid[3][3])
  */
 int checker_estable(int grid1[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid1[i][j] >= 4)
-                return (1);
-        }
-    }
-    return (0);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] >= 4)
+				return (1);
+		}
+	}
+	return (0);
 }
 
 /**
@@ -50,15 +50,15 @@ int checker_estable(int grid1[3][3])
  */
 void sum_grid(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
 }
 
 /**
@@ -69,41 +69,41 @@ void sum_grid(int grid1[3][3], int grid2[3][3])
  */
 void stabilizator(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
-    /* Convert the second grid in 0 to save the changes of grid1*/
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid2[i][j] = 0;
-        }
-    }
+	int i, j;
+	/* Convert the second grid in 0 to save the changes of grid1*/
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid2[i][j] = 0;
+		}
+	}
 
-    /* Make the sum by slot */
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid1[i][j] >= 4)
-            {
-                if (i - 1 >= 0) /* UP */
-                    grid2[i - 1][j] += 1;
+	/* Make the sum by slot */
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] >= 4)
+			{
+				if (i - 1 >= 0) /* UP */
+					grid2[i - 1][j] += 1;
 
-                if (j - 1 >= 0) /* LEFT */
-                    grid2[i][j - 1] += 1;
+				if (j - 1 >= 0) /* LEFT */
+					grid2[i][j - 1] += 1;
 
-                if (i + 1 <= 2) /* BOTTOM */
-                    grid2[i + 1][j] += 1;
+				if (i + 1 <= 2) /* BOTTOM */
+					grid2[i + 1][j] += 1;
 
-                if (j + 1 <= 2) /* RIGHT */
-                    grid2[i][j + 1] += 1;
+				if (j + 1 <= 2) /* RIGHT */
+					grid2[i][j + 1] += 1;
 
-                grid1[i][j] -= 4;
-            }
-        }
-    }
+				grid1[i][j] -= 4;
+			}
+		}
+	}
 
-    sum_grid(grid1, grid2);
+	sum_grid(grid1, grid2);
 }
 
 /**
@@ -114,12 +114,12 @@ void stabilizator(int grid1[3][3], int grid2[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    sum_grid(grid1, grid2);
+	sum_grid(grid1, grid2);
 
-    while (checker_estable(grid1))
-    {
-        printf("=\n");
-        _print_grid(grid1);
-        stabilizator(grid1, grid2);
-    }
+	while (checker_estable(grid1))
+	{
+		printf("=\n");
+		_print_grid(grid1);
+		stabilizator(grid1, grid2);
+	}
 }
