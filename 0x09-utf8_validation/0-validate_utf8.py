b5e713ed08a768316c97aa1fc447ca3f88e42b77
AraqueGD <camiaraquecaro17@gmail.com>
2020-09-23 23:07:45 -0500

    [Fix]Fix Logic

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
 """
-Validate utf-8
-
+    Valited utf-8
 """
+
+
 def validUTF8(data):
     """
-        Function Valid UTF-8
+     Function Valid UTF-8 with Bits
     """
-    for i in data:
-        bin = '{0:08b}'.format(i)
-        if len(bin) > 8:
-            return False
+    countBytes = 0
+    oneVer = 1 << 7
+    secondVer = 1 << 6
 
-    return True
+    for i in data:
+        mask_n_byte = 1 << 7
+        if countBytes == 0:
+            while mask_n_byte & i:
+                countBytes += 1
+                mask_n_byte = mask_n_byte >> 1
+            if countBytes == 0:
+                continue
+            if countBytes == 1 or countBytes > 4:
+                return False
+        else:
+            if not (i & oneVer and not (i & secondVer)):
+                return False
+        countBytes -= 1
+    if countBytes == 0:
+        return True
+    return False
