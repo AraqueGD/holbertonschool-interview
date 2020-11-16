11bc2e8035d38296062f3d8507c83f88e4c8f19e
unknown <camiaraquecaro17@gmail.com>
2020-11-16 17:15:32 -0500

    [ADD]README.md

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -8,20 +8,31 @@
 const request = require('request');
 const args = process.argv.slice(2);
 const url = 'https://swapi-api.hbtn.io/api/films/' + args[0] + '/';
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    const movie = JSON.parse(body);
-    for (const character of movie.characters) {
-      request(character, function (error2, response, body2) {
-        if (error) {
-          console.error(error2);
-        } else {
-          const person = JSON.parse(body2);
-          console.log(person.name);
-        }
-      });
+request(url, (error, response, body) => {
+    if (!error) {
+      const res = JSON.parse(body);
+      const characters = res.characters;
+      if (characters.length > 0) {
+        characterRequest(0, characters[0], characters, characters.length);
+      }
+    } else {
+      console.log(error);
     }
+  });
+  
+  function characterRequest (idx, urlChar, characters, limit) {
+    if (idx === limit) {
+      return;
+    }
+    request(urlChar, function (error, response, body) {
+      if (!error) {
+        const response = JSON.parse(body);
+        console.log(response.name);
+        idx++;
+        characterRequest(idx, characters[idx], characters, limit);
+      } else {
+        console.error('error:', error);
+      }
+    });
   }
-});
+  
