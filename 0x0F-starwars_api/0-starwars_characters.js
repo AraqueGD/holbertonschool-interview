856aadd0cb9cf6e731a6f52a6a7bcf02abbdaf73
unknown <camiaraquecaro17@gmail.com>
2020-11-16 17:09:50 -0500

    [ADD]0. Star Wars Characters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,25 +5,23 @@
     - You must use the Star wars API
     - You must use the module request
 */
-
-const request = require("request")
-const args = process.argv.slice(2)
-const url = "https://swapi-api.hbtn.io/api/films/" + args[0] + "/"
-
-request(url, function(error, respose, body) {
-    if (error) {
-        console.error(error)
-    } else {
-        const movie = JSON.parse(body)
-        for (const character of movie.characters) {
-            request(character, function(err, res, body2) {
-                if (err) {
-                    console.error(err)
-                } else {
-                    const person = JSON.parse(body2)
-                    console.log(person.name)
-                }
-            })
+const request = require('request');
+const args = process.argv.slice(2);
+const url = 'https://swapi-api.hbtn.io/api/films/' + args[0] + '/';
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    for (const character of movie.characters) {
+      request(character, function (error2, response, body2) {
+        if (error) {
+          console.error(error2);
+        } else {
+          const person = JSON.parse(body2);
+          console.log(person.name);
         }
+      });
     }
-})
+  }
+});
